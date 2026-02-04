703f3d89326de7e8e1e8d84e5df0543591d762de
Nicolas Boutet <amd3002@gmail.com>
2026-02-04 17:58:46 +0100

    Improve test coverage

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -7,24 +7,30 @@ import pytest
         ("cron"),
     ],
 )
-def test_packages_are_installed(host, name):
+def test_dependencies_are_installed(host, name):
     package = host.package(name)
     assert package.is_installed
 
 
+def test_geoipupdate_command_is_available(host):
+    cmd = host.run("/usr/local/bin/geoipupdate --version")
+    assert cmd.rc == 0
+
+
 @pytest.mark.parametrize(
-    "username,groupname,path",
+    "path,user,group,mode",
     [
-        ("root", "root", "/usr/local/bin/geoipupdate"),
-        ("root", "root", "/usr/local/etc/GeoIP.conf"),
+        ("/usr/local/bin/geoipupdate", "root", "root", 0o755),
+        ("/usr/local/etc/GeoIP.conf", "root", "root", 0o644),
     ],
 )
-def test_geoipupdate_files(host, username, groupname, path):
+def test_geoipupdate_files_exist(host, path, user, group, mode):
     config = host.file(path)
     assert config.exists
     assert config.is_file
-    assert config.user == username
-    assert config.group == groupname
+    assert config.user == user
+    assert config.group == group
+    assert config.mode == mode
 
 
 @pytest.mark.parametrize(
